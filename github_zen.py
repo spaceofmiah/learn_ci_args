51d3ec8d8a1f70e440ff99860a74e742c8ca300f
spaceofmiah <geek_200@outlook.com>
2021-05-13 03:04:29 +0100

    Initial commit

---
+++ github_zen.py
@@ -0,0 +1,16 @@
+"""
+    Codebase is a simplified approach on how to implement
+    command line argument in python with argparse library.
+
+    
+
+    ===================
+    python version: 3.8
+    code lesson url:
+
+    Project Description: Project interacts with github's
+    API and retrieve a provided number of github zen. It
+    can also save the zen's retrieved in a file if stated.
+    ===================
+"""
+
